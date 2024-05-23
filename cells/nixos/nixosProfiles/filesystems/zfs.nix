e9984bb1bf113b8ca0f4e044f03090ecc60a2d1e
Voob of Doom <vod@njk.li>
2024-05-23 18:59:34 +0200

    fix: deploy

--- cells/nixos/nixosProfiles/filesystems/zfs.nix
+++ cells/nixos/nixosProfiles/filesystems/zfs.nix
@@ -4,8 +4,6 @@
 {
   boot.kernel.sysctl."vm.swappiness" = 1;
 
-  boot.kernelParams = [ "zfs_force=1" ];
-
   boot.zfs.allowHibernation = lib.mkDefault true;
   boot.zfs.forceImportAll = lib.mkDefault false;
   boot.zfs.forceImportRoot = lib.mkDefault false;
@@ -21,7 +19,7 @@
   services.udev.extraRules = ''
     ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
   '';
-  boot.kernelParams =
+  boot.kernelParams = [ "zfs_force=1" ] ++
     lib.optional
       (cell.lib.isZfs config && config.deploy.params.zfsCacheMax != null)
       "zfs.zfs_arc_max=${config.deploy.params.zfsCacheMax}";
