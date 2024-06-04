07eb53e67c038fc482ac113098edb647e31ff2b8
Voob of Doom <vod@njk.li>
2024-06-04 20:21:55 +0200

    fix: fuck rust

--- cells/nixos/nixosProfiles/core/locale.nix
+++ cells/nixos/nixosProfiles/core/locale.nix
@@ -23,7 +23,8 @@ in
 
   # NOTE: https://sourceware.org/git/?p=glibc.git;a=blob;f=localedata/SUPPORTED
   # time.timeZone = mkDefault "Europe/Berlin";
-  services.automatic-timezoned.enable = true;
+  # services.automatic-timezoned.enable = true;
+  services.localtimed.enable = true;
 
   i18n.defaultLocale = mkDefault "en_US.UTF-8";
   i18n.supportedLocales = mkDefault [
