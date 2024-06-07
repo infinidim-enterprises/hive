731d3a945eea39b914f4e552ae497ef44118e333
Voob of Doom <vod@njk.li>
2024-06-07 15:59:08 +0200

    fix: tigervnc

--- cells/nixos/nixosProfiles/desktop/vncserver.nix
+++ cells/nixos/nixosProfiles/desktop/vncserver.nix
@@ -1,13 +1,14 @@
 { inputs, cell, ... }:
 
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 lib.mkMerge [
   (lib.mkIf config.services.xserver.displayManager.lightdm.enable {
+    # systemd.services.display-manager.path = [ pkgs.tigervnc ];
     services.xserver.displayManager.lightdm.extraConfig = ''
       [VNCServer]
       enabled=true
-      command=Xvnc -SecurityTypes None
+      command=${pkgs.tigervnc}/bin/Xvnc -SecurityTypes None
       port=44444
       listen-address=0.0.0.0
       width=1920
