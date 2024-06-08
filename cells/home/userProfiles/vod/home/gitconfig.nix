b111377fd0d5b78f107a307b1547fa8da0c09dde
Voob of Doom <vod@njk.li>
2024-06-08 15:34:28 +0200

    fix: programs.git.signing.key

--- cells/home/userProfiles/vod/home/gitconfig.nix
+++ cells/home/userProfiles/vod/home/gitconfig.nix
@@ -1,6 +1,7 @@
 {
-  # programs.git.userName = "Voob of Doom";
-  # programs.git.userEmail = "vod@njk.li";
+  programs.git.userName = "Voob of Doom";
+  programs.git.userEmail = "v@njk.li";
+  programs.git.signing.key = "1953CEBFBC2D71B3CA498433F7EFDE2FDC1E69C5";
   # programs.git.signing.key = "E3C4C12EDF24CA20F167CC7EE203A151BB3FD1AE";
   programs.git.extraConfig.github.user = "voobscout";
 }
