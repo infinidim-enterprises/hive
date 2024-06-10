31941a85169bc8880cd6f08238a2759b699d1bb3
Voob of Doom <vod@njk.li>
2024-06-10 17:17:27 +0200

    fix: rdp2vnc

---
+++ cells/nixos/nixosProfiles/desktop/rdpserver.nix
@@ -0,0 +1,63 @@
+{ inputs, cell, ... }:
+
+{ config, lib, pkgs, ... }:
+
+lib.mkMerge [
+  (lib.mkIf config.services.xserver.displayManager.lightdm.enable {
+    systemd.services.display-manager.path = [ pkgs.tigervnc ];
+    services.xserver.displayManager.lightdm.extraConfig = ''
+      [VNCServer]
+      enabled=true
+      command=Xvnc -SecurityTypes None
+      port=44444
+      listen-address=127.0.0.1
+      width=1920
+      height=1080
+      depth=24
+    '';
+  })
+
+  {
+    sops.secrets.xrdp-password = {
+      key = "xrdp-password";
+      sopsFile = ../../../secrets/sops/nixos-common.yaml;
+      neededForUsers = true;
+    };
+  }
+
+  {
+    networking.firewall.allowedTCPPorts = [ 3389 ];
+    containers.xrdp.autoStart = true;
+    containers.xrdp.ephemeral = true; # NOTE: Journal will not link to host
+    containers.xrdp.additionalCapabilities = [ "CAP_NET_BIND_SERVICE" ];
+
+    containers.xrdp.bindMounts.user-passwd.mountPoint = config.sops.secrets.xrdp-password.path;
+    containers.xrdp.bindMounts.user-passwd.hostPath = config.sops.secrets.xrdp-password.path;
+    containers.xrdp.bindMounts.user-passwd.isReadOnly = true;
+
+    containers.xrdp.config = {
+      users.users.rdp.hashedPasswordFile = config.sops.secrets.xrdp-password.path;
+      users.users.rdp.isNormalUser = true;
+
+      services.xrdp.enable = true;
+      services.xrdp.audio.enable = true;
+      # services.xrdp.openFirewall = true;
+      services.xrdp.extraConfDirCommands =
+        let
+          xrdp_vnc_ini = ''
+            [vnc]
+            name=LightDM
+            lib=libvnc.so
+            username=ask
+            password=ask
+            ip=127.0.0.1
+            port=44444
+          '';
+        in
+
+        ''
+          echo '${xrdp_vnc_ini}' >> $out/xrdp.ini
+        '';
+    };
+  }
+]
