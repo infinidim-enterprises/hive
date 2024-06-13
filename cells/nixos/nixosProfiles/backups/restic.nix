f470498faff07e2bdae5dc7c6bbcb80f7c83849a
Voob of Doom <vod@njk.li>
2024-06-13 16:26:27 +0200

    fix: deploy

--- cells/nixos/nixosProfiles/backups/restic.nix
+++ cells/nixos/nixosProfiles/backups/restic.nix
@@ -28,12 +28,18 @@ in
     format = "binary";
   };
 
+  sops.secrets.restic_passwd = {
+    key = "restic_passwd";
+    sopsFile = ../../../secrets/sops/online-storage-systems.yaml;
+  };
+
   services.restic.backups."home-${user}" = {
     inherit paths;
     initialize = true;
     timerConfig.OnCalendar = "hourly";
     timerConfig.Persistent = true;
     # environmentFile = config.sops.secrets.rclone_conf;
+    passwordFile = config.sops.secrets.restic_passwd.path;
     rcloneConfigFile = config.sops.secrets.rclone_conf.path;
     repository = "rclone:mega_50GB:/backups/${user}";
     pruneOpts = [
