aee4591ffca62ed171c76aab370be3a6e126d5bf
Voob of Doom <vod@njk.li>
2024-06-22 11:55:13 +0200

    fix: deploy

--- cells/home/homeProfiles/security/gpg.nix
+++ cells/home/homeProfiles/security/gpg.nix
@@ -5,8 +5,12 @@
   programs.gpg.mutableKeys = true;
   programs.gpg.mutableTrust = true;
 
-  programs.gpg.publicKeys.vod.source = ./382A371CFB344166F69076BE8587AB791475DF76_public_key.asc;
-  programs.gpg.publicKeys.vod.trust = "ultimate";
+  programs.gpg.publicKeys = [
+    {
+      source = ./382A371CFB344166F69076BE8587AB791475DF76_public_key.asc;
+      trust = "ultimate";
+    }
+  ];
 
   services.gpg-agent.sshKeys = [ "1953CEBFBC2D71B3CA498433F7EFDE2FDC1E69C5" ];
 }
