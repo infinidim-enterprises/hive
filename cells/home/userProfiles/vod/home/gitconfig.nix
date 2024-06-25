9796cfe28a1f90138ddfab920c0c3d9e267b7ab5
Voob of Doom <vod@njk.li>
2024-06-25 14:06:43 +0200

    fix: deploy

--- cells/home/userProfiles/vod/home/gitconfig.nix
+++ cells/home/userProfiles/vod/home/gitconfig.nix
@@ -1,7 +1,8 @@
+{ lib, ... }:
 {
   programs.git.userName = "Voob of Doom";
   programs.git.userEmail = "v@njk.li";
-  programs.git.signing.key = "382A371CFB344166F69076BE8587AB791475DF76";
+  programs.git.signing.key = lib.mkDefault "382A371CFB344166F69076BE8587AB791475DF76";
   # programs.git.signing.key = "E3C4C12EDF24CA20F167CC7EE203A151BB3FD1AE";
   programs.git.extraConfig.github.user = "voobscout";
 }
