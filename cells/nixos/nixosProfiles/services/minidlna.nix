69732c92242c9e8f90234cb11cc34f2922363a84
Voob of Doom <v@njk.li>
2024-09-27 17:29:49 +0200

    fix: minidlna conf

--- cells/nixos/nixosProfiles/services/minidlna.nix
+++ cells/nixos/nixosProfiles/services/minidlna.nix
@@ -5,7 +5,7 @@
 {
   services.minidlna.enable = true;
   services.minidlna.openFirewall = true;
-  services.minidlna.settings.mediaDirs = [ "/opt/media" ];
+  services.minidlna.settings.media_dir = [ "V,/opt/media" ];
   services.minidlna.settings.friendly_name = "dacha";
   services.minidlna.settings.inotify = "yes";
 }
