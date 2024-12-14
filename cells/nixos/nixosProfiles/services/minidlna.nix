23d5d54af790651ab0e071ae18fca183fe9677eb
Voob of Doom <v@njk.li>
2024-12-14 09:54:44 +0000

    fix: max_user_watches

--- cells/nixos/nixosProfiles/services/minidlna.nix
+++ cells/nixos/nixosProfiles/services/minidlna.nix
@@ -1,11 +1,13 @@
 { inputs, cell, ... }:
 
 { ... }:
-
+# fs.inotify.max_user_watches=524288
 {
   services.minidlna.enable = true;
   services.minidlna.openFirewall = true;
   services.minidlna.settings.media_dir = [ "V,/opt/media" ];
   services.minidlna.settings.friendly_name = "dacha";
   services.minidlna.settings.inotify = "yes";
+
+  boot.kernel.sysctl."fs.inotify.max_user_watches" = 524288;
 }
