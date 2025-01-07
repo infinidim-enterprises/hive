04b36f604731ee894b29051082cb5aed36427fb0
Voob of Doom <v@njk.li>
2025-01-07 17:23:47 +0000

    fix: tables and permissions

--- cells/nixos/nixosProfiles/networking/dhcp-ddns/postgresql.nix
+++ cells/nixos/nixosProfiles/networking/dhcp-ddns/postgresql.nix
@@ -6,80 +6,46 @@ with lib;
 let
   inherit (lib // builtins)
     concatMapStringsSep
+    mapAttrsToList
+    flatten
     mkAfter
     mkForce
+    imap0
     map;
 
-  inherit (pkgs) writeText;
+  dbSetup = concatStringsSep "\n" (imap0
+    (i: s: if i > 0 then "  ${s}" else s)
+    (flatten (mapAttrsToList
+      (n: v:
+        let
+          psql = "$PSQL --host=127.0.0.1 --no-password --username=${n} --dbname=${n}";
+          cmd = "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ${n}";
+        in
+        [ "${psql} --file=\"${v}\"" "${psql} --command='${cmd}'" ])
+      {
+        powerdns = "${pkgs.powerdns}/share/doc/pdns/schema.pgsql.sql";
+        kea = "${pkgs.kea}/share/kea/scripts/pgsql/dhcpdb_create.pgsql";
+      })));
 
-  powerdnsSqlSetup = writeText "powerdns-permissions.sql"
-    ((concatMapStringsSep "\n"
-      (s: "GRANT ALL ON" + " " + s + " " + "TO powerdns;")
-      [
-        "domains"
-        "domains_id_seq"
-        "records"
-        "records_id_seq"
-        "domainmetadata"
-        "domainmetadata_id_seq"
-        "comments"
-        "comments_id_seq"
-        "cryptokeys"
-        "cryptokeys_id_seq"
-        "tsigkeys"
-        "tsigkeys_id_seq"
-      ]) + "\n" + "GRANT SELECT ON supermasters TO powerdns;" + "\n");
-
-  keadhcpSqlSetup = writeText "kea-permissions.sql"
-    ((concatMapStringsSep "\n"
-      (s: "GRANT ALL ON" + " " + s + " " + "TO kea;")
-      [
-        "dhcp4_options"
-        "dhcp6_options"
-        "dhcp_option_scope"
-        "host_identifier_type"
-        "hosts"
-        "ipv6_reservations"
-        "lease4"
-        "lease4_stat"
-        "lease6"
-        "lease6_stat"
-        "lease6_types"
-        "lease_hwaddr_source"
-        "lease_state"
-        "logs"
-        "schema_version"
-        "dhcp4_options_option_id_seq"
-        "dhcp6_options_option_id_seq"
-        "hosts_host_id_seq"
-        "ipv6_reservations_reservation_id_seq"
-      ]) + "\n" + "SET TIMEZONE = 'CET';" + "\n");
 in
 {
   config = mkMerge [
     {
       systemd.services.postgresql = with config.services.postgresql; {
         preStart = mkAfter ''
-          [[ -e  "${dataDir}/.first_startup" ]] && touch "${dataDir}/.first_startup_user" || true
+          if test -e "${dataDir}/.first_startup"; then
+            touch "${dataDir}/.first_startup_user"
+          fi
         '';
         postStart =
           mkAfter ''
             if test -e "${dataDir}/.first_startup_user"; then
-              $PSQL --host=127.0.0.1 --no-password --username=powerdns --dbname=powerdns --file="${pkgs.powerdns}/share/doc/pdns/schema.pgsql.sql"
-              $PSQL --host=127.0.0.1 --no-password --username=powerdns --dbname=powerdns --command='GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO powerdns'
-
-              $PSQL --host=127.0.0.1 --no-password --username=kea --dbname=kea --file="${pkgs.kea}/share/kea/scripts/pgsql/dhcpdb_create.pgsql"
-              $PSQL --host=127.0.0.1 --no-password --username=kea --dbname=kea --command='GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO kea'
-
+              ${dbSetup}
               rm -f "${dataDir}/.first_startup_user"
             fi
           '';
-
-        # $PSQL -f "${powerdnsSqlSetup}" -d powerdns
-        # $PSQL -f  ${keadhcpSqlSetup} -d kea
         # $PSQL -tAc "alter user kea password 'kea'"
         # $PSQL -tAc "alter user powerdns password 'powerdns'"
-
       };
 
       services.postgresql = {
@@ -97,7 +63,6 @@ in
         authentication = mkForce ''
           local all all trust
           host all all 127.0.0.1/32 trust
-          # host all all localhost trust
         '';
 
         ensureDatabases = [ "powerdns" "kea" ];
