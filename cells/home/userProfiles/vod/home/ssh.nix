dc513cea9e49c37d06c3bbe46b8d9ff9cf0bd533
Voob of Doom <v@njk.li>
2025-01-08 03:19:01 +0000

    fix: port forwarding ssh

--- cells/home/userProfiles/vod/home/ssh.nix
+++ cells/home/userProfiles/vod/home/ssh.nix
@@ -54,12 +54,13 @@ mkMerge [
         # "maintenance* maintenance*.0*.njk.li".port = 65522;
         "kakrafoon kakrafoon.njk.*" = defaults_njk // {
           port = 65522;
-          remoteForwards = [{
-            bind.address = "127.0.0.1";
+          localForwards = [{
+            # bind.address = "127.0.0.1";
             bind.port = 5432;
             host.address = "127.0.0.1";
             host.port = 5432;
           }];
+
         };
         "bitbucket.org" = defaults_git;
         "github.com" = defaults_git;
