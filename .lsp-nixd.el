7f512ee77606f2404a0ad88042085a97a23fbe2f
Voob of Doom <v@njk.li>
2025-03-01 10:21:53 +0000

    fix: refactor emacs config

---
+++ .lsp-nixd.el
@@ -0,0 +1,22 @@
+;;; .lsp-nixd.el --- Description -*- lexical-binding: t; -*-
+
+
+(defun dir-locals/setup-nixd ()
+  (when (getenv "PRJ_ROOT")
+    (let* ((prj_root (getenv "PRJ_ROOT"))
+           (username (getenv "USER"))
+           (hostname (with-temp-buffer (call-process "hostname" nil t nil)
+                                       (string-trim (buffer-string))))
+           (flakeref (format "(builtins.getFlake \"%s\")" prj_root))
+           (options-nixos (format "%s.nixosConfigurations.nixos-%s.options" flakeref hostname))
+           (options-home-manager (format "%s.homeConfigurations.home-nixd.options" flakeref)))
+      (setq lsp-nix-nixd-nixpkgs-expr "import <nixpkgs> { }"
+            lsp-nix-nixd-nixos-options-expr options-nixos
+            lsp-nix-nixd-home-manager-options-expr options-home-manager))))
+
+(add-hook 'envrc-mode-hook #'dir-locals/setup-nixd)
+
+
+
+;; (provide '.lsp-nixd)
+;;; .lsp-nixd.el ends here
